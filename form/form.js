72ab1273019bab0840a1c9286e52a648c881e064
ly <nitefullsand@163.com>
2016-03-29 15:01:46 +0800

    我的第一个表单

---
+++ form/form.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+function fz()
+{	 var rtl=document.getElementById("rtl");
+
+     var index=rtl.selectedIndex;
+	 alert(rtl.selectedIndex)
+	 if(index==0){
+	 window.location.href="http://www.sina.com.cn/"}
+	 if(index==1){
+	 window.location.href="https://www.baidu.com/s?ie=UTF-8&wd=w3cschool"}
+	 if(index==2){
+	 window.location.href="http://tieba.baidu.com/"}
+	 if(index==3){
+	 window.location.href="http://yun.baidu.com/?ref=PPZQ"}
+}
