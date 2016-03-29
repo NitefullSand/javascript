0c5000b0eb26b9c63e258b9900556e18f885d9ad
ly <nitefullsand@163.com>
2016-03-29 17:18:25 +0800

    修改后链接可以访问的表单

--- form/form.js
+++ form/form.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 function fz()
-{	 var rtl=document.getElementById("rtl");
+{	 var x=document.getElementById("x");
 
-     var index=rtl.selectedIndex;
-	 alert(rtl.selectedIndex)
+     var index=x.selectedIndex;
+	 
 	 if(index==0){
 	 window.location.href="http://www.sina.com.cn/"}
 	 if(index==1){
-	 window.location.href="https://www.baidu.com/s?ie=UTF-8&wd=w3cschool"}
+	 window.location.href="http://www.w3school.com.cn/"}
 	 if(index==2){
 	 window.location.href="http://tieba.baidu.com/"}
 	 if(index==3){
